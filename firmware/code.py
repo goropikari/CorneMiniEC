6a6531bb102619934d5f597fd4e03871ef49f532
goropikari <goropikari56@gmail.com>
2023-01-20 23:36:45 +0900

    update qwerty

--- firmware/code.py
+++ firmware/code.py
@@ -76,7 +76,7 @@ keyboard.keymap = [
     #     KC.Q   , KC.W   , KC.E   , KC.R   , KC.T   ,  KC.Y   , KC.U   , KC.I   , KC.O   , KC.P   ,
     #     KC.A   , KC.S   , KC.D   , KC.F   , KC.G   ,  KC.H   , KC.J   , KC.K   , KC.L   , KC.SCLN,
     #     KC.Z   , KC.X   , KC.C   , KC.V   , KC.B   ,  KC.N   , KC.M   , KC.COMM, KC.DOT , KC.SLSH,
-    #                       EscGUI , LowMins, KC.LCTL,  RaiEnt , Sands  , KC.NO
+    #                       EscGUI , LowMins, KC.LCTL,  RaiEnt , Sands  , KC.GRV
     # ],
     # lower
     [
